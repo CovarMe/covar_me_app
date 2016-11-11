ee87d5bc2ba0b2570b1f5028f7fdc622217aadba
Santiago Paz <santiago.l.paz@gmail.com>
2016-11-11 22:48:53 +0100

    Randomizeit

--- cvapp.py
+++ cvapp.py
@@ -2,6 +2,11 @@ from flask import Flask
 from flask_pymongo import PyMongo
 from flask.views import View
 from flask import render_template
+import random
+names = ["Donald","Yoda","LeBron James", 
+         "Theme", "Davide", "David", 
+         "Jonas", "Santi", "World", 
+         "Insect Overlords"]
 
 app = Flask(__name__)
 mongo = PyMongo(app)
@@ -9,7 +14,6 @@ mongo = PyMongo(app)
 @app.route('/')
 
 def home_page():
-    name = "james"
-    return render_template('index.html',name=name)
+    return render_template('index.html',name = random.choice(names))
 
 
