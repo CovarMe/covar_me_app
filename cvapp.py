840a5db5bf8e967996334e202fe7360f3494294b
Santiago Paz <santiago.l.paz@gmail.com>
2016-11-11 22:28:05 +0100

    Adding James the man.com

--- cvapp.py
+++ cvapp.py
@@ -1,7 +1,15 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask.views import View
+from flask import render_template
 
 app = Flask(__name__)
 mongo = PyMongo(app)
+
+@app.route('/')
+
+def home_page():
+    name = "james"
+    return render_template('index.html',name=name)
 
 
