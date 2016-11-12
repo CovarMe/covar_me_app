1ceed61851e7d95fe7735babddcef9c0c21abe02
Westermann <westermann93@googlemail.com>
2016-11-12 22:13:38 +0100

    Add tuktuk and setup controllers file

---
+++ controllers.py
@@ -0,0 +1,21 @@
+from flask import render_template
+
+import random
+names = ["Donald","Yoda","LeBron James", 
+         "Theme", "Davide", "David", 
+         "Jonas", "Santi", "World", 
+         "Insect Overlords"]
+
+def show_homepage():
+    return render_template('index.html', name = random.choice(names))
+
+def show_registration_form():
+    return render_template('registration.html')
+
+def register_new_user():
+    def login():
+        if request.method == 'POST':
+            do_the_login()
+        else:
+            show_the_login_form()
+    return ""
--- cvapp.py
+++ cvapp.py
@@ -3,7 +3,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 from flask import Flask
-from flask import render_template
+from flask import request
 from flask_bower import Bower
 from mongoengine import connect
 
@@ -14,12 +14,6 @@ from mongo_schemata import *
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-import random
-names = ["Donald","Yoda","LeBron James", 
-         "Theme", "Davide", "David", 
-         "Jonas", "Santi", "World", 
-         "Insect Overlords"]
-
 # create a Flask application
 app = Flask(__name__)
 # inject bower (for front-end resource management)
@@ -27,9 +21,20 @@ Bower(app)
 # connect to MongoDB
 connect(host='mongodb://' + os.environ.get('MONGO_HOST') + '/' + os.environ.get('MONGO_DBNAME'))
 
+from controllers import *
+
 @app.route('/')
 def home_page():
-    return render_template('index.html',name = random.choice(names))
+    return show_homepage() 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        return register_new_user()
+    elif request.method == 'GET':
+        return show_registration_form()
+    else: 
+        return 404
 
 @app.route('/portfolio/<username>-<portfolio_id>')
 def show_portfolio(username, portfolio_id):
