3b879be954ec727fdf3639107cc49c2c775c5198
Westermann <westermann93@googlemail.com>
2016-11-12 11:13:24 +0100

    Add mongo-engine and data models

--- cvapp.py
+++ cvapp.py
@@ -1,19 +1,30 @@
+# import all the required stuff
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask
-from flask_pymongo import PyMongo
-from flask.views import View
 from flask import render_template
+from mongoengine import connect
+
+# set up the data models defined for mongo
+from mongo_schemata import *
+
+# load environment variables from .env file
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
 import random
 names = ["Donald","Yoda","LeBron James", 
          "Theme", "Davide", "David", 
          "Jonas", "Santi", "World", 
          "Insect Overlords"]
 
+# create a Flask application
 app = Flask(__name__)
-mongo = PyMongo(app)
+# connect to MongoDB
+connect(host='mongodb://' + os.environ.get('MONGO_HOST') + '/' + os.environ.get('MONGO_DBNAME'))
 
 @app.route('/')
 
 def home_page():
     return render_template('index.html',name = random.choice(names))
-
-print(app)
---
+++ mongo_schemata.py
@@ -0,0 +1,17 @@
+import datetime
+from mongoengine import *
+
+class Stock(Document):
+    ticker = StringField(required = True, max_length = 4)
+
+
+class Portfolio(Document):
+    name = StringField(required = True, max_length = 120)
+    stocks = ListField(ReferenceField(Stock))
+
+
+class User(Document):
+    email = EmailField(unique = True)
+    password = StringField(required = True, default = True, min_length = 8)
+    timestamp = DateTimeField(default = datetime.datetime.now())
+    portfolios = ListField(ReferenceField(Portfolio))
