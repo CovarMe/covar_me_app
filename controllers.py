12e0110afe54b1bef1c6dd92fc8649d3a5fded02
Westermann <westermann93@googlemail.com>
2016-11-18 16:16:44 +0100

    Add portfolio ticker selection frontend

--- controllers.py
+++ controllers.py
@@ -7,9 +7,11 @@ names = ["Donald","Yoda","LeBron James",
          "Jonas", "Santi", "World", 
          "Insect Overlords"]
 
+
 def show_homepage():
     return render_template('index.html', 
                            name = random.choice(names))
+
 
 def show_portfolio(username, portfolio_id):
     opentsdb_res = opentsdb_query(
@@ -29,12 +31,16 @@ def show_portfolio(username, portfolio_id):
         name = random.choice(names),
         chart_data = chart_data)
 
+
 def show_new_portfolio_form(username):
     return render_template('new_portfolio.html',
-                           username = username)
+                           username = username,
+                           tickers = ['ABC','MSFT'])
+
 
 def show_registration_form():
     return render_template('registration.html')
+
 
 def register_new_user():
     def login():
